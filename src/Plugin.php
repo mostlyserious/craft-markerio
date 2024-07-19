ae54c8872d2c2b40d4050838648506506decb396
Mike Moreau <mike@mostlyserious.io>
2024-07-19 16:32:51 -0500

    Do not set settings property

--- src/Plugin.php
+++ src/Plugin.php
@@ -28,7 +28,6 @@ class Plugin extends BasePlugin
 
     public array $widgetConfig = [];
     public string $widgetConfigScript = '';
-    public ?Settings $settings = null;
     public string $project = '';
 
     public const WIDGET_SHIM = '!function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);';
@@ -44,7 +43,6 @@ class Plugin extends BasePlugin
     {
         parent::init();
 
-        $this->settings = $this->getSettings();
         $this->project = $this->settings->getProject();
         $this->widgetConfig = $this->createWidgetConfig();
         $this->widgetConfigScript = $this->createWidgetConfigScript($this->widgetConfig);
